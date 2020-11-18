7d205ed63b3474944cfc83a78487e02a6d3d23cd
Abylaikhan <66362171+AbylaikhanT@users.noreply.github.com>
2020-11-18 11:27:04 +0600

    Update ItemsOfLombards.vue

--- src/components/ItemsOfLombards.vue
+++ src/components/ItemsOfLombards.vue
@@ -81,34 +81,28 @@ export default {
 .vip {
   padding-top: 40px;
   padding-bottom: 0;
-
   .items {
     display: flex;
     flex-wrap: wrap;
     max-width: 960px;
   }
-
   .item {
     margin-right: 25px;
     margin-top: 15px;
   }
-
   @media screen and (max-width: 768px) {
     padding: 80px 20px 0;
     .item {
       margin: 15px 0 0;
     }
   }
-
   .more {
     display: flex;
     justify-content: center;
     margin-top: 40px;
-
     &.hidden {
       display: none;
     }
-
     button {
       width: 220px;
       height: 49px;
@@ -120,7 +114,6 @@ export default {
       border-radius: 6px;
     }
   }
-
   h3 {
     margin-bottom: 25px;
     font-size: 24px;
@@ -133,7 +126,6 @@ export default {
     margin-top: 20px;
     padding: 0 20px;
     width: 100%;
-
     .item {
       margin-right: 0;
     }
